3fe07f8e8bbf4f4f8ed48fa5b6c3b6fd3594636d
Habibul Islam <habibulislam6862@gmail.com>
2026-07-07 06:13:32 +0600

    Revert "test: bad commit that should be blocked"
    This reverts commit a98408ab82661827be687408bc1f5c20d734694a.

--- src/features/auth/components/BadComponent.tsx
+++
@@ -1,7 +0,0 @@
-// intentionally broken: unused import + missing type
-import axios from 'axios'
-
-export default function BadComponent() {
-  const x: any = axios.get('/foo')
-  return <div>{x}</div>
-}
